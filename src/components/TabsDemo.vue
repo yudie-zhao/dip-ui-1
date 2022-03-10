70a1fbb3eb7047e462cce4e3df4c1bea5ab0c80d
yudie <1641500852@qq.com>
2022-03-10 19:12:42 +0800

    tab

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,105 +1,22 @@
 <template>
-  <div>
-    Tabs 的文档
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-    <p>tabs 的文档</p>
-  </div>
+  <div>Tabs 示例</div>
+  <h1>示例1</h1>
+  <Tabs v-model:selected="x">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
+<script lang="ts">
+import Tab from '../lib/Tab.vue'
+import Tabs from '../lib/Tabs.vue'
+import {ref} from "vue";
+export default{
+  components:{
+    Tab,Tabs
+  },
+  setup(){
+    const x=ref('导航1')
+    return {x}
+  }
+}
+</script>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="dip-tabs">
+    <div class="dip-tabs-nav" ref="container">
+      <div class="dip-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (el) navItems[index] = el }"
+           @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      <div class="dip-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="dip-tabs-content">
+      <component :is="current" :key="current.props.title" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Tab from './Tab.vue'
+import {
+  computed, onMounted, onUpdated, ref
+} from 'vue'
+export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
+  setup(props, context) {
+    const navItems=ref<HTMLDivElement[]>([])
+    const indicator=ref<HTMLDivElement>(null)
+    const container=ref<HTMLDivElement>(null)
+    const x=()=>{
+      const divs=navItems.value
+      const result=divs.filter((div)=>div.classList.contains('selected'))[0]
+      const {width}= result.getBoundingClientRect()
+      indicator.value.style.width=width+'px'
+      const {left:left1}=container.value.getBoundingClientRect()
+      const {left:left2}=result.getBoundingClientRect()
+      const left=left2-left1
+      indicator.value.style.left=left+'px'
+    }
+    onMounted(x)
+    onUpdated(x)
+    const defaults=context.slots.default()
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab')
+      }
+    })
+    const current = computed(() => {
+      return defaults.find(tag => tag.props.title === props.selected)
+    })
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
+    return {
+      defaults,
+      titles,
+      current,
+      select,
+      navItems,
+      indicator,
+      container
+    }
+  }
+}
+</script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.dip-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position:relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+    &-indicator{
+      position:absolute;
+      height:3px;
+      background:$blue;
+      left:0;
+      bottom:-1px;
+      width:100px;
+      transition:all 250ms;
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
