3b435d73ab2990608c903dab25b3d98a1724719e
yudie <1641500852@qq.com>
2022-03-14 16:28:52 +0800

    github page

--- src/router.ts
+++ src/router.ts
@@ -5,20 +5,23 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import install from './markdown/install.md'
+import getStarted from './markdown/get-started.md'
+import intro from './markdown/intro.md'
 import './index.scss'
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
 const history=createWebHashHistory()
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = String => h(Markdown, { content:String, key: String })
 
 export const router=createRouter({
 history:history,
 routes:[{path: '/',component:Home},
 {path:'/doc',component:Doc,children: [
                 {path:'',redirect:'/doc/intro'},
-        { path: "intro", component: md('intro') },
-        { path: "get-started", component: md('get-started') },
-        { path: "install", component: md('install') },
+        { path: "intro", component: md(intro) },
+        { path: "get-started", component: md(getStarted) },
+        { path: "install", component: md(install) },
                 {path:'switch',component:SwitchDemo},
                 {path:'button',component:ButtonDemo},
                 {path:'dialog',component:DialogDemo},
