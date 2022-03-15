1ba80bdba065844ea02ed38ad12019f0e606b066
yudie <1641500852@qq.com>
2022-03-15 08:45:28 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M main &&
+git remote add origin git@github.com:yudie-zhao/dip-ui-website-1.git &&
+git push -f -u origin main &&
+cd - &&
+echo https://yudie-zhao.github.io/dip-ui-website-1/index.html
